b7a5c9912c3d1c48373d73643636c64af84d5412
kelvinmayeye <kevmayeye97@gmail.com>
2024-05-01 14:29:15 +0300

    fix: register account fixed

--- farmers_laravel_api/app/Http/Controllers/AuthController.php
+++ farmers_laravel_api/app/Http/Controllers/AuthController.php
@@ -11,46 +11,41 @@ use Illuminate\Validation\Rule;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $loginUserData = $request->validate([
-            'username'=>'required|string',
-            'password'=>'required|min:8'
+            'username' => 'required|string',
+            'password' => 'required|min:8'
         ]);
-        $user = User::where('username',$loginUserData['username'])->first();
-        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
-            return response()->json([
-                'message' => 'Wrong Username or Password'
-            ],401);
+        $user = User::where('username', $loginUserData['username'])->first();
+        if (!$user || !Hash::check($loginUserData['password'],$user->password)) {
+            return response()->json([ 'message' => 'Wrong Username or Password'], 401);
         }
-        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
-        return response([
-            'user' => $user,
-            'token' => $token
-        ], 200);
+        $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
+        return response(['user' => $user,'token' => $token], 200);
     }
 
-    public function register(Request $request){
-        $registerUserData = $request->validate([
-            'name'=>'required|string',
-            'username'=>'required|string|unique:users',
-            'role' => ['required', 'string', Rule::in(['farmer', 'customer'])],
-            'email'=>'required|string|email|unique:users',
-            'password'=>'required|min:8'
-        ]);
-        $registerUserData['password'] = Hash::make($registerUserData['password']);
-      //  return Response()->json($registerUserData);
-        $user = User::create($registerUserData);
-        return response()->json([
-            'message' => 'User Created ',
-        ]);
+    public function register(Request $request)
+    {
+        try {
+            $registerUserData = $request->validate([
+                'name' => 'required|string',
+                'username' => 'required|string|unique:users',
+                'role' => ['required', 'string', Rule::in(['farmer', 'customer'])],
+                'email' => 'required|string|email|unique:users',
+                'password' => 'required|confirmed|min:8'
+            ]);
+            $registerUserData['password'] = Hash::make($registerUserData['password']);
+            User::create($registerUserData);
+        } catch (\Exception $e) {
+            return response(['message' => $e->getMessage()], 201);//successful but something is wrong
+        }
+        return response(['message' => 'Welcome Your Account created successful'], 200);
     }
 
-    public function logout(){
-        Log::info("tried to logout");
+    public function logout()
+    {
         auth()->user()->tokens()->delete();
-
-        return response()->json([
-            "message"=>"logged out"
-        ]);
+        return response()->json(['message' => 'logged out']);
     }
 }
